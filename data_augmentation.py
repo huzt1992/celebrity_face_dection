c3847a5b174ab2d91aa5b83569b615f59539065a
huzt1992 <huzt@outlook.com>
2023-03-29 01:27:35 -0400

    enlarge dataset

---
+++ data_augmentation.py
@@ -0,0 +1,69 @@
+import os
+import random
+import numpy as np
+from PIL import Image, ImageEnhance, ImageOps
+
+# Define the folder containing the input images
+input_folder = r"D:\MCIT\581\extra project\val\brad_pitt"
+
+# Define the folder to export the augmented images to
+output_folder = r"C:\Users\Huzt\Downloads\New folder"
+
+# Define the number of augmented images to generate per input image
+num_augmentations = 2
+
+# Define the range of rotation angles in degrees
+rotation_range = (-0.5, 0.5)
+
+# Define the range of shear angles in degrees
+shear_range = (-0.5, 0.5)
+
+# Define the range of color enhancements
+brightness_range = (0.5, 1.5)
+contrast_range = (0.5, 1.5)
+sharpness_range = (0.5, 1.5)
+
+# Define the range of cropping ratios
+crop_range = (0.8, 1.0)
+
+# Define a list of image file extensions to process
+extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif"]
+
+# Loop through all the image files in the input folder
+for filename in os.listdir(input_folder):
+    if any(filename.lower().endswith(ext) for ext in extensions):
+        # Open the input image file
+        image = Image.open(os.path.join(input_folder, filename)).convert('RGB')
+        # Loop through the number of augmentations to generate
+        for i in range(num_augmentations):
+            # Apply random rotation
+            angle = random.uniform(*rotation_range)
+            rotated_image = image.rotate(angle)
+
+            # Apply random shear
+            shear = random.uniform(*shear_range)
+            sheared_image = rotated_image.transform(
+                rotated_image.size, Image.AFFINE, (1, shear, 0, 0, 1, 0))
+
+            # Apply random color enhancement
+            brightness = random.uniform(*brightness_range)
+            contrast = random.uniform(*contrast_range)
+            sharpness = random.uniform(*sharpness_range)
+            enhancer = ImageEnhance.Brightness(
+                ImageEnhance.Contrast(
+                    ImageEnhance.Sharpness(sheared_image).enhance(sharpness)
+                ).enhance(contrast)
+            ).enhance(brightness)
+
+            # Apply random cropping
+            crop_ratio = random.uniform(*crop_range)
+            crop_size = (int(enhancer.width * crop_ratio),
+                         int(enhancer.height * crop_ratio))
+            cropped_image = ImageOps.fit(
+                enhancer, crop_size, centering=(0.5, 0.5))
+
+            # Save the augmented image to the output folder
+            output_filename = os.path.splitext(filename)[0] + \
+                f"_aug{i:02d}.jpg"
+            output_path = os.path.join(output_folder, output_filename)
+            cropped_image.save(output_path)
---
+++ random_pick.py
@@ -0,0 +1,19 @@
+import os
+import random
+import shutil
+
+src_folder = r'D:\Humans'
+dst_folder = r'D:\MCIT\581\extra project\val\ppl'
+num_files = 150
+
+# Get a list of all the files in the source folder
+files = os.listdir(src_folder)
+
+# Randomly select 'num_files' files from the list
+selected_files = random.sample(files, num_files)
+
+# Copy the selected files to the destination folder
+for file in selected_files:
+    src_path = os.path.join(src_folder, file)
+    dst_path = os.path.join(dst_folder, file)
+    shutil.copy(src_path, dst_path)
